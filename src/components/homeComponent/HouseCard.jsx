d7079687be910f440ec20f1ec90e8bf8427fe460
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2023-07-17 22:46:47 +0600

    [DONE] : home search

---
+++ src/components/homeComponent/HouseCard.jsx
@@ -0,0 +1,5 @@
+const HouseCard = () => {
+  return <div></div>;
+};
+
+export default HouseCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,12 @@
 import Banner from '../components/homeComponent/Banner';
+import HouseSearch from '../components/homeComponent/HouseSearch';
 
 const Home = () => {
   return (
     <div>
       <Banner />
 
-      <div className="grid grid-cols-7 ">
-        <div className="px-4 col-span-2">
-          <input type="text" className="px-2 py-1 my-2 block w-full" />
-          <input type="text" className="px-2 py-1 my-2 block w-full" />
-          <input type="text" className="px-2 py-1 my-2 block w-full" />
-          <input type="text" className="px-2 py-1 my-2 block w-full" />
-          <input type="text" className="px-2 py-1 my-2 block w-full" />
-        </div>
-        <div className="col-span-5">
-          <div className=" grid grid-cols-4 gap-2"></div>
-        </div>
-      </div>
+      <HouseSearch />
     </div>
   );
 };
