ce83189ba11565183948f30eb5b4b4de7db317e8
SanyFaysal <tfoysalahmed@gmail.com>
2024-04-27 16:29:47 +0600

    modified house card

--- src/components/OwnerDashboardComponent/MyHouseTableRow.jsx
+++ src/components/OwnerDashboardComponent/MyHouseTableRow.jsx
@@ -36,11 +36,11 @@ const MyHouseTableRow = ({ house }) => {
     <tr>
       <td>
         <div className="flex items-center space-x-3">
-          <div className="avatar">
+          {/* <div className="avatar">
             <div className="mask mask-squircle w-12 h-12">
               <img src={houseImg} alt="Avatar Tailwind CSS Component" />
             </div>
-          </div>
+          </div> */}
           <div>
             <div className="font-bold">{house?.name}</div>
             <div className="text-sm opacity-50">{house?.city}</div>
